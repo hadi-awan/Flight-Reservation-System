b6747fa22acf2570269f207f744063ee2b70e158
hadi-awan <87779346+hadi-awan@users.noreply.github.com>
2021-12-01 14:53:48 -0500

    Update SeatOccupiedException.java

--- src/SeatOccupiedException.java
+++ src/SeatOccupiedException.java
@@ -1,9 +1,9 @@
-// This is a custom error class that is thrown when a seat is already taken/occupied
+/*
+ * A custom error class that is thrown when a seat is already taken/occupied
+ */
 
-class SeatOccupiedException extends Exception
-{
-    public SeatOccupiedException(String message)
-    {
+class SeatOccupiedException extends Exception {
+    public SeatOccupiedException(String message) {
         super(message);
     }
 }

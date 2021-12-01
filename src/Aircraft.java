1ca1594c3d1b61bb77644422f7ce7d4a79554f81
hadi-awan <87779346+hadi-awan@users.noreply.github.com>
2021-12-01 14:12:19 -0500

    Update Aircraft.java

--- src/Aircraft.java
+++ src/Aircraft.java
@@ -1,5 +1,4 @@
-public final class Aircraft implements Comparable<Aircraft>
-{
+public final class Aircraft implements Comparable<Aircraft> {
     int numEconomySeats;
     int numFirstClassSeats;
     String[][] seatLayout;
@@ -7,63 +6,52 @@ public final class Aircraft implements Comparable<Aircraft>
     String model;
     int rows = 4;
 
-    public Aircraft(int seats, String model)
-    {
+    public Aircraft(int seats, String model) {
         this.numEconomySeats = seats;
         this.numFirstClassSeats = 0;
         this.model = model;
     }
 
-    public Aircraft(int economy, int firstClass, String model)
-    {
+    public Aircraft(int economy, int firstClass, String model) {
         this.numEconomySeats = economy;
         this.numFirstClassSeats = firstClass;
         this.model = model;
     }
 
-    public int getNumSeats()
-    {
+    public int getNumSeats() {
         return numEconomySeats;
     }
 
-    public int getTotalSeats()
-    {
+    public int getTotalSeats() {
         return numEconomySeats + numFirstClassSeats;
     }
 
 
-    public int getNumFirstClassSeats()
-    {
+    public int getNumFirstClassSeats() {
         return numFirstClassSeats;
     }
 
-    public String getModel()
-    {
+    public String getModel() {
         return model;
     }
 
-    public void setModel(String model)
-    {
+    public void setModel(String model) {
         this.model = model;
     }
 
-    public void setNumEconomySeats(int numEconomySeats)
-    {
+    public void setNumEconomySeats(int numEconomySeats) {
         this.numEconomySeats = numEconomySeats;
     }
 
 
-    public int getRows()
-    {
-        if (getTotalSeats() <= rows)
-        {
+    public int getRows() {
+        if (getTotalSeats() <= rows) {
             this.rows = 2;
         }
         return rows;
     }
 
-    public String[][] getSeatLayout()
-    {
+    public String[][] getSeatLayout() {
         // Returns a 2D array for the seat-layout of the aircraft.
         int seats = getNumSeats();
         int columns = seats / getRows();
@@ -72,18 +60,14 @@ public final class Aircraft implements Comparable<Aircraft>
         return seatLayout;
     }
 
-    public void setSeatLayout(int capacity)
-    {
+    public void setSeatLayout(int capacity) {
         // Labels the seats for the aircraft.
         String[] labels = {"A", "B", "C", "D"};
         int columns = capacity / rows;
-        for (int col = 1; col <= columns; col++)
-        {
-            for (int row = 0; row < rows; row++)
-            {
+        for (int col = 1; col <= columns; col++) {
+            for (int row = 0; row < rows; row++) {
                 String label = labels[row];
-                if(numFirstClassSeats>0 && (col * rows) < numFirstClassSeats)
-                {
+                if(numFirstClassSeats>0 && (col * rows) < numFirstClassSeats) {
                     label += "+";
                 }
                 this.seatLayout[col - 1][row] =  col + label;
@@ -91,8 +75,7 @@ public final class Aircraft implements Comparable<Aircraft>
         }
     }
 
-    public void print()
-    {
+    public void print() {
         System.out.println("Model: " + model + "\t Economy Seats: " + numEconomySeats + "\t First Class Seats: " + numFirstClassSeats);
     }
 
@@ -102,26 +85,20 @@ public final class Aircraft implements Comparable<Aircraft>
      * greater  than object aircraft and 0 if they are equal.
      */
     @Override
-    public int compareTo(Aircraft aircraft)
-    {
-        if (this.numEconomySeats == aircraft.numEconomySeats)
-        {
-            if (this.numFirstClassSeats == aircraft.numFirstClassSeats)
-            {
+    public int compareTo(Aircraft aircraft) {
+        if (this.numEconomySeats == aircraft.numEconomySeats) {
+            if (this.numFirstClassSeats == aircraft.numFirstClassSeats) {
                 return 0;
             }
-            else if (this.numFirstClassSeats > aircraft.numFirstClassSeats)
-            {
+            else if (this.numFirstClassSeats > aircraft.numFirstClassSeats) {
                 return 1;
             }
-            else
-            {
+            else {
                 return -1;
             }
 
         }
-        else if (this.numEconomySeats > aircraft.numEconomySeats)
-        {
+        else if (this.numEconomySeats > aircraft.numEconomySeats) {
             return 1;
         }
         return -1;

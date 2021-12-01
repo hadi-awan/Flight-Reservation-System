b5c16d807220a6baa41c5ea9bac0feb34f5f788e
hadi-awan <87779346+hadi-awan@users.noreply.github.com>
2021-12-01 14:16:45 -0500

    Update Flight.java

--- src/Flight.java
+++ src/Flight.java
@@ -1,3 +1,8 @@
+/*
+ *  Models an airline flight. In this simple system, all flights originate from Toronto.
+ *  A simple flight has only economy seats.
+ */
+
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.TreeMap;

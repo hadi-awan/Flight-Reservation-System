9fc68977c0f5bd75bfd910dde910096f65d31389
hadi-awan <87779346+hadi-awan@users.noreply.github.com>
2021-12-01 14:09:30 -0500

    Update Flight.java
    Models a simple flight that has only economy seats, originating from Toronto

--- src/Flight.java
+++ src/Flight.java
@@ -2,19 +2,12 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.TreeMap;
 
-/*
- *  Class to model an airline flight. In this simple system, all flights originate from Toronto
- *  This class models a simple flight that has only economy seats
- */
-public class Flight
-{
-    public enum Status
-    {
+public class Flight {
+    public enum Status {
         // A special class that returns a status of a flight.
         DELAYED, ONTIME, ARRIVED, INFLIGHT
     };
 
-    // Fields
     String flightNum;
     String airline;
     String origin, dest;
@@ -22,13 +15,11 @@ public class Flight
     Status status; 
     int flightDuration;
     Aircraft aircraft;
-    protected int passengers; // count of (economy) passengers on this flight - initially 0
+    protected int passengers; 
     protected ArrayList<Passenger> manifest;
     protected TreeMap<String, Passenger> seatMap;
 
-    public Flight()
-    {
-        // Constructor method which initializes all the fields.
+    public Flight() {
         this.flightNum = null;
         this.airline = null;
         this.dest = null;
@@ -44,9 +35,7 @@ public class Flight
         this.seatMap = new TreeMap<>();
     }
 
-    public Flight(String flightNum, String airline, String dest, String departure, int flightDuration, Aircraft aircraft)
-    {
-        // Constructor method which initializes all the fields.
+    public Flight(String flightNum, String airline, String dest, String departure, int flightDuration, Aircraft aircraft) {
         this.flightNum = flightNum;
         this.airline = airline;
         this.dest = dest;
@@ -61,150 +50,104 @@ public class Flight
         this.seatMap = new TreeMap<>();
     }
 
-    public String getFlightNum()
-    {
-        // Accessor method
+    public String getFlightNum() {
         return flightNum;
     }
 
-    public void setFlightNum(String flightNum)
-    {
-        // Mutator method
+    public void setFlightNum(String flightNum) {
         this.flightNum = flightNum;
     }
 
-    public String getAirline()
-    {
-        // Accessor method
+    public String getAirline() {
         return airline;
     }
 
-    public void setAirline(String airline)
-    {
-        // Mutator method
+    public void setAirline(String airline) {
         this.airline = airline;
     }
 
-    public String getOrigin()
-    {
-        // Accessor method
+    public String getOrigin() {
         return origin;
     }
 
-    public void setOrigin(String origin)
-    {
-        // Mutator method
+    public void setOrigin(String origin) {
         this.origin = origin;
     }
 
-    public String getDest()
-    {
-        // Accessor method
+    public String getDest() {
         return dest;
     }
 
-    public void setDest(String dest)
-    {
-        // Mutator method
+    public void setDest(String dest) {
         this.dest = dest;
     }
 
-    public String getDepartureTime()
-    {
-        // Accessor method
+    public String getDepartureTime() {
         return departureTime;
     }
 
-    public void setDepartureTime(String departureTime)
-    {
-        // Mutator method
+    public void setDepartureTime(String departureTime) {
         this.departureTime = departureTime;
     }
 
-    public Status getStatus()
-    {
-        // Accessor method
+    public Status getStatus() {
         return status;
     }
 
-    public void setStatus(Status status)
-    {
-        // Mutator method
+    public void setStatus(Status status) {
         this.status = status;
     }
 
-    public int getFlightDuration()
-    {
-        // Accessor method
+    public int getFlightDuration() {
         return flightDuration;
     }
 
-    public void setFlightDuration(int dur)
-    {
-        // Mutator method
+    public void setFlightDuration(int dur) {
         this.flightDuration = dur;
     }
 
-    public int getPassengers()
-    {
-        // Accessor method
+    public int getPassengers() {
         return passengers;
     }
 
-    public void setPassengers(int passengers)
-    {
-        // Mutator method
+    public void setPassengers(int passengers) {
         this.passengers = passengers;
     }
 
-    public ArrayList<Passenger> getManifest()
-    {
-        // Accessor method
+    public ArrayList<Passenger> getManifest() {
         return manifest;
     }
 
-    public void setManifest(ArrayList<Passenger> manifest)
-    {
-        // Mutator method
+    public void setManifest(ArrayList<Passenger> manifest) {
         this.manifest = manifest;
     }
 
-    FlightType getFlightType()
-    {
-        // Accessor method which returns the type of flight (the default is MEDIUMHAUL)
+    FlightType getFlightType() {
+        // Returns the type of flight (the default is MEDIUMHAUL)
         return FlightType.MEDIUMHAUL;
     }
 
-    public void printPassengerManifest()
-    {
-        //Prints all the passengers on the flight
-        for (Passenger passenger : manifest)
-        {
+    public void printPassengerManifest() {
+        for (Passenger passenger : manifest) {
             System.out.println(passenger.toString());
         }
     }
 
-    public void printSeats()
-    {
-        //Uses the 2D array seatLayout[][] in the aircraft object for this flight to print seats
+    public void printSeats() {
+        // Uses the 2D array seatLayout[][] in the aircraft object for this flight to print seats
         String[][] seatLayout = aircraft.getSeatLayout();
-        for (int row = 0; row < seatLayout[1].length; row++)
-        {
-            for (int col = 0; col < seatLayout.length; col++)
-            {
+        for (int row = 0; row < seatLayout[1].length; row++) {
+            for (int col = 0; col < seatLayout.length; col++) {
                 String seat = seatLayout[col][row];
-                if (seatMap.containsKey(seat))
-                {
+                if (seatMap.containsKey(seat)) {
                     seat = "XX";
                 }
                 System.out.print(seat+ " ");
             }
-            if (row == 1)
-            {
+            if (row == 1) {
                 System.out.println("\n");
             }
-            else
-            {
+            else {
                 System.out.println();
             }
         }
@@ -214,31 +157,22 @@ public class Flight
         System.out.println("XX = Occupied" + "\t" + "+ = First Class");
     }
 
-    public boolean seatsAvailable()
-    {
-        // Checks to see if there is room on this flight by comparing
-        // the current passenger count with the aircraft max capacity of economy seats
+    public boolean seatsAvailable() {
+        // Checks to see if there is room on this flight 
         return passengers <= aircraft.getNumSeats();
     }
 
-    /*
-     * Cancel a seat of the given passenger
-     */
-    public void cancelSeat(Passenger p) throws PassengerNotInManifestException
-    {
+    public void cancelSeat(Passenger p) throws PassengerNotInManifestException {
         // Check if the passenger actually exists
-        for (Map.Entry<String, Passenger> entry : seatMap.entrySet())
-        {
+        for (Map.Entry<String, Passenger> entry : seatMap.entrySet()) {
             String key = entry.getKey();
             Passenger passenger = entry.getValue();
-            if (passenger.equals(p))
-            {
+            if (passenger.equals(p)) {
                 // If the passenger exists, then remove passengers from manifest and seat map
                 manifest.remove(passenger);
                 seatMap.remove(key);
                 // If there are more than 0 passengers, then reduce the passenger count
-                if (passengers > 0)
-                {
+                if (passengers > 0) {
                     passengers--;
                 }
                 return;
@@ -248,28 +182,22 @@ public class Flight
         throw new PassengerNotInManifestException("Passenger " + p.toString() + " not found");
     }
 
-    public void reserveSeat(Passenger p, String seat) throws DuplicatePassengerException, SeatOccupiedException
-    {
+    public void reserveSeat(Passenger p, String seat) throws DuplicatePassengerException, SeatOccupiedException {
         // Reserves a seat on this flight ensuring that there are no duplicate passengers
         // Checks if the passenger already exists.
-        for (Passenger passenger : manifest)
-        {
-            if (passenger.equals(p))
-            {
+        for (Passenger passenger : manifest) {
+            if (passenger.equals(p)) {
                 // If the passenger already exists, then we do not reserve a seat and throw a DuplicatePassengerException.
                 throw new DuplicatePassengerException("Duplicate Passenger " + p.getName() + " " + p.getPassport());
             }
         }
-        if (seatsAvailable())
-        {
+        if (seatsAvailable()) {
             // Checks if a seat is occupied.
-            if (seatMap.containsKey(seat))
-            {
+            if (seatMap.containsKey(seat)) {
                 // If a seat is taken, then we do not reserve a seat and throw a SeatOccupiedException
                 throw new SeatOccupiedException("Seat " + seat + " already occupied");
             }
-            else
-            {
+            else {
                 // If the seat is not occupied, then add the passenger on board and increase the passenger count.
                 manifest.add(p);
                 seatMap.put(seat, p);
@@ -279,11 +207,7 @@ public class Flight
     }
 
     @Override
-    public String toString()
-    {
-        // Overriding the toString method of class Objects.
-        // Return a string consisting of the airline, the flight-number, the destination,
-        // the departure-time, the flight-duration, and the status.
+    public String toString() {
         return airline + "\t Flight:  " + flightNum + "\t Dest: " + dest + "\t Departing: " + departureTime + "\t Duration: " + flightDuration + "\t Status: " + status;
     }
 }

159476203372f6f471b162ff30a8c88ce1819972
hadi-awan <87779346+hadi-awan@users.noreply.github.com>
2021-12-01 14:01:40 -0500

    A custom error class that is thrown when a given passenger is already in the flight

--- src/DuplicatePassengerException.java
+++ src/DuplicatePassengerException.java
@@ -1,5 +1,3 @@
- // This is a custom error class that is thrown when a given passenger is already in the flight.
-
 class DuplicatePassengerException extends Exception
 {
     public DuplicatePassengerException(String message)

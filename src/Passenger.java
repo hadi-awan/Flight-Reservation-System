bbf4d2adfc64ebba6ca175f5d5ddacdc219ae706
hadi-awan <87779346+hadi-awan@users.noreply.github.com>
2021-12-01 14:49:43 -0500

    Update Passenger.java

--- src/Passenger.java
+++ src/Passenger.java
@@ -1,105 +1,78 @@
-import java.util.Objects;
-
 /*
  * Represents  a passenger object
  * Contains attributes such as passenger name, passport and seatNumber
  */
 
+import java.util.Objects;
+
 public class Passenger
 {
-    // Instance variables
     private String name;
     private String passport;
-
     private String seat;
     private String seatType;
 
-    public Passenger(String name, String passport, String seat, String seatType)
-    {
-        // Constructor method which initializes the instance variables.
+    public Passenger(String name, String passport, String seat, String seatType) {
         this.name = name;
         this.passport = passport;
         this.seat = seat;
         this.seatType = seatType;
     }
 
-    public String getName()
-    {
-        // Accessor method
+    public String getName() {
         return name;
     }
 
-    public String getPassport()
-    {
-        // Accessor method
+    public String getPassport() {
         return passport;
     }
 
-    public String getSeat()
-    {
-        // Accessor method
+    public String getSeat() {
         return seat;
     }
 
-    public void setSeat(String seat)
-    {
-        // Mutator method
+    public void setSeat(String seat) {
         this.seat = seat;
     }
 
-    public String getSeatType()
-    {
-        // Accessor method
+    public String getSeatType() {
         return seatType;
     }
 
-    public void setSeatType(String seatType)
-    {
-        // Mutator method
+    public void setSeatType(String seatType) {
         this.seatType = seatType;
     }
 
 
     @Override
-    public int hashCode()
-    {
-        // If a class overrides equals, it must override hashCode.
-        // If two objects are equal, then their hashCode values must be equal as well.
-        // Therefore, we take a prime number (7) as a hash.
-        // Finally, we return hash.
+    public int hashCode() {
+        // If a class overrides equals, it must override hashCode
+        // If two objects are equal, then their hashCode values must be equal as well
+        // Therefore, we take a prime number (7) as a hash
+        // Finally, we return hash
         int hash = 7;
         return hash;
     }
 
     @Override
-    public boolean equals(Object obj)
-    {
-        // Overriding the equals method of class Objects.
-        // Comparing two objects of the class Passenger.
-        // If the two objects are equal, then return true.
-        // If the two objects are not equal, then return false.
-        if (this == obj)
-        {
+    public boolean equals(Object obj) {
+        // Overriding the equals method of class Objects
+        // Comparing two objects of the class Passenger
+        if (this == obj) {
             return true;
         }
-        if (obj == null || getClass() != obj.getClass())
-        {
+        if (obj == null || getClass() != obj.getClass()) {
             return false;
         }
         final Passenger passenger = (Passenger) obj;
-
-        if (!Objects.equals(this.name, passenger.name))
-        {
+        if (!Objects.equals(this.name, passenger.name)) {
             return false;
         }
         return Objects.equals(this.passport, passenger.passport);
     }
 
     @Override
-    public String toString()
-    {
-        // Overriding the toString method of class Objects.
-        // Returns a string containing the name of the passenger, their passport-number, and their seat-number
+    public String toString() {
         return name + "\t" + passport + "\t" + seat;
     }
 }

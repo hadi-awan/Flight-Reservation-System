b9f43a4b26d377060cf082aea578976e8e81328b
hadi-awan <87779346+hadi-awan@users.noreply.github.com>
2021-12-01 14:13:36 -0500

    Update FlightFullException.java

--- src/FlightFullException.java
+++ src/FlightFullException.java
@@ -1,9 +1,9 @@
-// This is a custom error class that is thrown when a given flight is full.
+/* 
+* A custom error class that is thrown when a given flight is full.
+*/
 
-class FlightFullException extends Exception
-{
-    public FlightFullException(String message)
-    {
+class FlightFullException extends Exception {
+    public FlightFullException(String message) {
         super(message);
     }
 }

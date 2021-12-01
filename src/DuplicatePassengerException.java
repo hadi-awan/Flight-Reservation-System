f12060e596659e4c1a7273f7a0f1778c564c39c7
hadi-awan <87779346+hadi-awan@users.noreply.github.com>
2021-12-01 14:15:25 -0500

    Update DuplicatePassengerException.java

--- src/DuplicatePassengerException.java
+++ src/DuplicatePassengerException.java
@@ -1,3 +1,7 @@
+/*
+ * A custom error class that is thrown when a given passenger is already in the flight
+ */
+
 class DuplicatePassengerException extends Exception {
     public DuplicatePassengerException(String message) {
         super(message);

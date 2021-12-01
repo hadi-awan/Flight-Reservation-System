7c1e50efe18c6827d8d1da2b18e5cac60f56b75f
hadi-awan <87779346+hadi-awan@users.noreply.github.com>
2021-12-01 13:43:46 -0500

    Create PassengerNotInManifestException.java

---
+++ src/PassengerNotInManifestException.java
@@ -0,0 +1,9 @@
+// This is a custom error class that is thrown when a given passenger does not exist in the flight manifest
+
+class PassengerNotInManifestException extends Exception
+{
+    public PassengerNotInManifestException(String message)
+    {
+        super(message);
+    }
+}

f93399ac4600458c9214b4f7bb3d25b1d2f0603c
hadi-awan <87779346+hadi-awan@users.noreply.github.com>
2021-12-01 14:45:14 -0500

    Update FlightType.java

--- src/FlightType.java
+++ src/FlightType.java
@@ -1,8 +1,8 @@
-// A special class which gives a group of constants which represent
-// the different flight-types.
+/* 
+ * A special class which gives a group of constants which represent the different flight-types.
+ */
 
-public enum FlightType
-{
+public enum FlightType {
     SHORTHAUL,
     MEDIUMHAUL,
     LONGHAUL
